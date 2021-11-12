90ede7d99b570cc81143fbc91bd5271b28a8063f
ShreyanshTripathi123 <93584912+ShreyanshTripathi123@users.noreply.github.com>
2021-11-12 16:08:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+for(var i=0; i<=45; i++){
+  if(marks[i]>=45){
+    console.log(marks[i]);
+  }
+}
+
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
